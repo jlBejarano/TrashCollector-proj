b2bf0cb7a28f09c3c2a585b128638f3c9ab3eae3
jorge bejarano <jorge.bejarano@yahoo.com>
2020-05-22 20:08:23 -0400

    added employee trash pick up model

---
+++ TrashCollector/Models/EmployeeTrashPU.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class EmployeeTrashPU
+    {
+        public Employee Employee { get; set; }
+        public DateTime PUDate { get; set; }
+        public List<Customer> customers { get; set; }
+    }
+}
