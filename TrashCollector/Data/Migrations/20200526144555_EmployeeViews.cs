36074cf79122a6b34c66c221f6b32814db714671
jorge bejarano <jorge.bejarano@yahoo.com>
2020-05-26 10:52:19 -0400

    added employeeview migration

---
+++ TrashCollector/Data/Migrations/20200526144555_EmployeeViews.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class EmployeeViews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "16ca23bb-a3c9-4253-a427-209977bd4050");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "56f23f47-fc5f-4d0c-859d-5c3fa3aa50a5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9add1339-3153-4b1c-93c9-bc839724902f");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "EmployeeId");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "50a10c04-7d7d-4b9c-8a62-a45a9aa48f98", "39d28d7e-e336-4c7b-b141-00384da182c9", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c5abca19-7dbf-421e-93ef-4807d3633ef2", "27711ad2-297e-4be5-a550-7f5bb74ef1d0", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b978dee0-554e-4676-af29-70d0476225ae", "490d1679-0438-48f5-a223-48d3a62ded0d", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "50a10c04-7d7d-4b9c-8a62-a45a9aa48f98");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b978dee0-554e-4676-af29-70d0476225ae");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c5abca19-7dbf-421e-93ef-4807d3633ef2");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9add1339-3153-4b1c-93c9-bc839724902f", "a2196734-36e8-44e2-aece-bf02dc4321fb", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "56f23f47-fc5f-4d0c-859d-5c3fa3aa50a5", "4be2c6f1-fdd7-4d70-8f91-e2afa23fbe31", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "16ca23bb-a3c9-4253-a427-209977bd4050", "9e139136-13f6-4f91-af38-5de219462955", "Admin", "ADMIN" });
+        }
+    }
+}
